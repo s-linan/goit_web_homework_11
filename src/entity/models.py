2d4ff4528ee82235fbf1b89e8a1f140aa207c1e8
Andrii Lypak <say.elsword@gmail.com>
2024-04-01 08:44:59 +0300

    comm/fresh_start

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, Date
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey, func
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
@@ -17,3 +17,20 @@ class Contact(Base):
     birthday = Column(Date, nullable=False)
     additional_data = Column(String(250), nullable=True)
     completed = Column(Boolean, default=False)
+    created_at = Column(Date, default=func.now(), nullable=True)
+    updated_at = Column(Date, default=func.now(), onupdate=func.mow(), nullable=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+    user = relationship("User", backref="contacts", lazy="joined")
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), nullable=False, unique=True, index=True)
+    email = Column(String(50), nullable=False, unique=True, index=True)
+    password = Column(String(255), nullable=False, index=True)
+    avatar = Column(String(255), nullable=True, index=True)
+    refresh_token = Column(String(255), nullable=True)
+    created_at = Column(Date, default=func.now())
+    updated_at = Column(Date, default=func.now(), onupdate=func.mow())
